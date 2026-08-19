b094bef1825bcf2550124c54ae67d5af068321bc
Ubuntu <ubuntu@ip-172-31-25-22.ap-northeast-1.compute.internal>
2026-08-19 05:14:19 +0000

    first submission 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   root to: "homes#top"
-  get "homes/about"
+  get 'home/about' => 'homes#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
